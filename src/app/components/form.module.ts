7e70ece0cd6892e13bd3db59a550065f5997cf65
Candido Hernandez <candidohm99cursos@gmail.com>
2024-03-07 21:19:49 -0600

    formulario

--- src/app/components/form.module.ts
+++ src/app/components/form.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// import { FormsModule } from '@angular/forms';
+
 import { AboutFormComponent } from './about-form/about-form.component';
 import { ExperienciaFormComponent } from './experiencia-form/experiencia-form.component';
 import { FormacionFormComponent } from './formacion-form/formacion-form.component';
